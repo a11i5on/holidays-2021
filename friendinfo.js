23b21cc24ae174ee3e7442e36e20e304ee1251b7
Allison <allisonl@lyft.com>
2021-12-14 15:51:20 -0800

    Timelines + basic form validation

--- friendinfo.js
+++ friendinfo.js
@@ -18,11 +18,21 @@ function checkEntry(){
 	var name = document.getElementById("name").value; 
 	var pwd = document.getElementById("pwd").value;
 
-	storage.setItem("name", name); 
-	storage.setItem("pwd", pwd); 
-	storage.setItem("time_elapsed_yrs", data[name].time_elapsed_yrs);
-	storage.setItem("time_elapsed_mos", data[name].time_elapsed_mos); 
-	storage.setItem("msg", data[name].msg); 
-	window.location.href = "index.html"; 
+	// Form validation 
+	if (!(name in data)) { 
+		alert("Incorrect name: please try again.");
+	} 
+	else if (pwd != data[name].pwd) {
+		console.log(pwd); 
+		console.log(data[name]); 
+		alert("Incorrect password: please ensure mm-yyyy string format."); 
+	} 
+	else {
+		storage.setItem("name", name); 
+		storage.setItem("time_elapsed_yrs", data[name].time_elapsed_yrs);
+		storage.setItem("time_elapsed_mos", data[name].time_elapsed_mos); 
+		storage.setItem("msg", data[name].msg); 
+		window.location.href = "index.html";
+	} 
 }
 
