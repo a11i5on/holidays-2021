c724fb89b9e88bd886a0098798c8e1abe3e281ad
Allison <allisonl@lyft.com>
2021-12-16 13:01:21 -0800

    tidying up code
    who knows what i broke tho

---
+++ card.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.demo-card', { duration: 800 });
+sr.reveal('.ending', { duration: 800 });
+
+document.getElementById("holidayHeader").innerHTML = "Happy Holidays, " + localStorage.getItem("name") + "!";
+document.getElementById("personalizedMsg").innerHTML = localStorage.getItem("msg");
+
+var img = document.createElement("img");
+img.style.cssText = "width:90%;display:block;margin-left:auto;margin-right:auto;width: 50%;margin-bottom:1rem;"; 
+img.src = "photos/friends/" + localStorage.name + ".jpg";
+
+var div = document.getElementById("friendPhoto");
+div.appendChild(img);
