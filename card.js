81615840ccbfe2a32f821b82377c604c61759695
Allison <allisonl@lyft.com>
2021-12-17 12:33:59 -0800

    added lazy loading to fix image issue

--- card.js
+++ card.js
@@ -2,10 +2,15 @@ window.sr = ScrollReveal({ reset: true });
 sr.reveal('.demo-card', { duration: 800 });
 sr.reveal('.ending', { duration: 800 });
 
+var lazyLoadInstance = new LazyLoad({
+  // Your custom settings go here
+});
+
 document.getElementById("holidayHeader").innerHTML = "Happy Holidays, " + localStorage.getItem("name") + "!";
 document.getElementById("personalizedMsg").innerHTML = localStorage.getItem("msg");
 
 var img = document.createElement("img");
+img.className = 'lazy'; 
 img.style.cssText = "width:90%;display:block;margin-left:auto;margin-right:auto;width: 50%;margin-bottom:1rem;"; 
 img.src = "photos/friends/" + localStorage.name + ".jpg";
 
